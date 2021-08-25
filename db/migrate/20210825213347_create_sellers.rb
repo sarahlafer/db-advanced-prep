d3267c358cc0944f59149c1eab6f6245ad4baa51
Sarah Lafer <svlafer@gmail.com>
2021-08-26 00:08:26 +0200

    setup

---
+++ db/migrate/20210825213347_create_sellers.rb
@@ -0,0 +1,14 @@
+class CreateSellers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :sellers do |t|
+      t.string :csv_id
+      t.string :zip_code_prefix
+      t.string :city
+      t.string :state
+      t.string :name
+
+      t.timestamps
+    end
+    add_index :sellers, :csv_id, unique: true
+  end
+end
